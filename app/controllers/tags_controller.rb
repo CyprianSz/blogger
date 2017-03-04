9097186aeb2afc2d8b9e1ead09f31cf93656e2dc
CyprianS <szwajka.cyprian@gmail.com>
2017-03-04 19:06:24 +0100

    Blog

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  before_filter :require_login, only: [:destroy]
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag \"#{@tag.name}\" Delated!"
+
+    redirect_to tags_path
+  end
+end
